52091ed1b034ff72979fa53dec03abdcefae1ad1
Mike Duran <mike.duran@hotmail.de>
2021-01-16 17:27:57 +0100

    server client construction

---
+++ client/client.py
@@ -0,0 +1,84 @@
+import socket
+import os
+import argparse
+import threading # we will create 2 threads: Receivinf msgs and Send msgs.
+
+
+
+class Client():
+    def __init__(self,host,port,name):
+        super().__init__()
+        self.host=host
+        self.port=port
+        self.name=name
+        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #to communicate with other devices
+    
+    def start(self):
+        print("[*] Connecting to server: " +str(self.host) + "," + str(self.port))
+        
+        try:
+            self.sock.connect((self.host, self.port))
+            print("[+] Successfuly connected to server: " +str(self.host) + "," + str(self.port))
+        except Exception as e :
+            print("[-] Failed to connect to server " +str(self.host) + "," + str(self.port))
+            exit(1)
+
+        #create send and receive
+        send=Send(args.host,args.p,self.sock)
+        receive=Receive(args.host,args.p,self.sock)
+
+        #start them
+        send.start()
+        receive.start()
+
+
+class Send(threading.Thread):
+    def __init__(self,host,port,personal_socket):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.sock=personal_socket
+        pass
+    
+    def run(self):
+        while True:
+            message= input("Message to send to server: ")
+            self.sock.sendall(message.encode('ascii'))
+
+
+class Receive(threading.Thread):
+    def __init__(self,host,port,personal_socket):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.sock=personal_socket
+        pass
+    
+    def run(self):
+        while True:
+            message = self.sock.recv(1024)
+            if message:
+                print("\n"+message.decode('ascii'))
+            else:
+                # Server has closed the socket, exit the program
+                print('[*] lost connection to the server')
+                print('[*] Quitting...')
+                self.sock.close()
+                os._exit(0)
+                
+                pass
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Chatroom Server')
+    parser.add_argument('host', help='Interface the server listens at')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+
+    # Create and start client
+    client=Client(args.host,args.p,"Mike")
+    client.start()
+
+
---
+++ server/server.py
@@ -0,0 +1,132 @@
+import threading # so that i can create a thread for each user
+import socket
+import os
+import argparse
+
+#Specific server that handles a specific client
+class ServerSocket(threading.Thread):
+
+    def __init__(self, client, sockname, server):
+        super().__init__()
+        self.client = client
+        self.sockname = sockname
+        self.server = server
+
+    def run(self):
+        while True: #a true loop inside the specific thread for the client.
+
+            message=self.client.recv(1024).decode('ascii')# waiting for client message
+
+            if message:
+                print('{} says {!r}'.format(self.sockname, message))
+                self.server.broadcast(message, self.sockname)
+            
+            else:
+                # Client has closed the socket, exit the thread
+                print('{} has closed the connection'.format(self.sockname))
+                self.client.close()
+                self.server.remove_connection(self)
+                return
+
+    def send(self, message):
+        msg=message + "\n"
+        self.client.send(msg.encode('ascii'))
+        pass
+
+
+
+
+#ACTUAL SERVER 
+class Server(threading.Thread):
+
+    def __init__(self, host, port):
+        super().__init__()
+        self.connections = []
+        self.host = host
+        self.port = port
+
+    def run(self):
+        # creation of the socket to communicate with other devices
+        sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #SOCK_STREAM because we use TCP
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #to bind the socket object to a host and a port on the server machine
+        sock.bind((self.host, self.port))
+
+        sock.listen(1) # this is a listening socket. not a connecting one.
+        print('Listening at', sock.getsockname())
+
+        while True:
+            #Accepting a new connection from a client
+            client, client_ip = sock.accept() #waits till client connects
+            print("[+] New Client connection from: ",str(client_ip))
+            #create a new thread for this client
+            server_socket = ServerSocket(client, client_ip, self)
+            # Start new thread
+            server_socket.start()
+        # Add thread to active connections
+            self.connections.append(server_socket)
+            print('[+] Ready to receive messages from', client.getpeername()) # return client socket adress on the other end of connection.
+
+
+    def broadcast(self, message, source):
+        print("[*] Start of broadcasting message of " +str(source))
+        for connection in self.connections:
+            # Send to all connected clients except the source client
+            if connection.sockname != source:
+                connection.send(message.encode('ascii'))
+                print("[+] Sending to " + str(connection.sockname))
+    
+
+    def send(self,message,source):
+        source=int(source)
+        print("[*] Sending message to "+ str(self.connections[source].sockname))
+        try:
+            self.connections[source].send(message.encode('ascii'))
+            print("[+] Message Sended to " + str(self.connections[source].sockname))
+        except Exception :
+            print("[-] Couldn't send message to: " + str(self.connections[source].sockname))
+        pass
+
+
+# function that is the thread that manages the sending of the server
+def server_managing(server):
+
+    while True:
+        ipt = input('')
+        if ipt == 'q':
+            print('[*] Closing all connections...')
+            for connection in server.connections:
+                try:
+                    print("[*] Closing " + str(connection.sockname))
+                    connection.client.sendall("QUIT".encode('ascii'))
+                    connection.client.close()
+                    print("[+]" +str(connection.sockname)+" successfuly closed")
+                except Exception as e :
+                    print("[-] Closing "+str(connection.sockname)+" failed.")
+            
+            print('[*] Shutting down the server...')
+            os._exit(0)
+        elif ipt == 'show clients' or ipt=='clients':
+            for connection in server.connections:
+                print(connection.sockname)
+        elif ipt =="send":
+            target=input("Target : ")
+            msg=input("Message: ")
+            print("sending msg to "+ str(server.connections[int(target)].sockname))
+            server.connections[int(target)].client.sendall(msg.encode('ascii'))
+            pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Chatroom Server')
+    parser.add_argument('host', help='Interface the server listens at')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+
+    # Create and start server thread
+    server = Server(args.host, args.p)
+    server.start()
+
+    server_managing = threading.Thread(target = server_managing, args = (server,))
+    server_managing.start()
+
