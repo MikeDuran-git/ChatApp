30312969230eb3eac5faa97b9017869880bb0e15
Mike Duran <mike.duran@hotmail.de>
2021-02-01 12:07:32 +0100

    messaging

--- main.py
+++ main.py
@@ -21,30 +21,14 @@ Window.softinput_mode = "below_target"
 
 
 
-class MyRecycleView(RecycleView):
-    def __init__(self, **kwargs): 
-        super(MyRecycleView, self).__init__(**kwargs) 
-        self.data = [] 
-    def add_message(self,msg):
-        self.data.append({'text':"{}".format(str(msg.text))})
-        pass
-
-
-
-
 class MyBoxLayout(BoxLayout):
-    
-    #myrecicleview=ObjectProperty(None)
+    scrollview=ObjectProperty(None)
     msg=ObjectProperty(None)
     msg_output=ObjectProperty(None)
+
     def send(self):
         if self.msg.text != "":
-            # newlabel=Label()
-            # newlabel.text=self.msg.text 
-            # newlabel.size_hint_y=None
-            # newlabel.height=newlabel.texture_size[1]
-            # newlabel.text_size= (self.width,None)
-            self.msg_output.text+= self.msg.text+"\n" 
+            self.msg_output.text+=self.msg.text+"\n"            
             print(self.msg.text)
             self.msg.text=""
 
